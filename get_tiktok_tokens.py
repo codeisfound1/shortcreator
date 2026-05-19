11866a0e85fc7dd129df03e5b1983260f63ed261
codeisfound1 <c.odeisfound@gmail.com>
2026-05-19 14:59:59 +0700

    Update get_tiktok_tokens.py

--- get_tiktok_tokens.py
+++ get_tiktok_tokens.py
@@ -8,7 +8,7 @@ import requests
 # ── Fill these in ──────────────────────────────────────────────
 CLIENT_KEY    = "sbawtfufg4q6a68j4z"
 CLIENT_SECRET = "dWi0TDfOnv1pxl68PHQdqGgBxmgrPM2N"
-CODE          = "ZZiJcF9BUr43qygLULVVbwxlPCL1W8IW6j28ztgaDQi1PIZDjtfJzu0U49naRdUZAMsliCC_vHnse2YfQTgzljo3WuIl4unBvef_xwDXXeOjecjdiZBiAaInxLrQOVTSQUolIAyREHdEOzTvZi79fvVaAXINuMQlbfCq8GtYREQSESwoQT-0GgpXIFUHv9P6aM4XwKF4kAR4haqiEFt5nOZUSZ4MoO_luZmfmA%2Av%216216.s1"   # the code=... from the browser URL
+CODE          = "9wyCbkfyVKV4bfcX5JWIyA9MTlPyoBNDEug5q1toQMgWNCzW8_1l-6ApKs412uTkTtOGOSKK7yUB-IgXI-R48gslQf9GVhg5uEL52s0lOtLArPzKh-wZE5-PI56WU7DUPxYoowdCVCDHhucbVtalk87IjP5sPV0iSQF9sp3Fvn9alFr-kB7p1S-n_TX6cHEe-uQmXJ1RH9lMENv7E1vNTOHsJnzLCYd1e3rhlQ%2Av%216242.s1"   # the code=... from the browser URL
 REDIRECT_URI  = "https://oauth.pstmn.io/v1/callback"
 # ───────────────────────────────────────────────────────────────
 
